a31f7ee119b138600f9ae20c07ff7d82c83929dc
Александр <kalyghnii@gmail.com>
2023-07-27 10:08:24 +0500

    fix

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -8,28 +8,29 @@ import util.Util;
 
 public class Calculator {
     private final List<Item> items  = new LinkedList<>();
-    public void start(int quantity, Scanner sc) {
-        addItems(sc);
+    public void start(int quantity, Scanner scanner) {
+        addItems(scanner);
         calculatePrice(quantity);
     }
 
-    private void addItems(Scanner sc) {
+    private void addItems(Scanner scanner) {
         while(true) {
             System.out.println("Введите товар");
-            String name = sc.next();
+            String name = scanner.next();
             System.out.println("Введите цену в формате: рубли,копейки");
             try {
-                double price = sc.nextDouble();
-                if (price <= 0) throw new NegativeNumberException("Введено отрицательное число");
+                double price = scanner.nextDouble();
+                if (price < 0) throw new NegativeNumberException("Введено отрицательное число");
                 items.add(new Item(name, price));
             } catch (InputMismatchException e) {
                 System.out.println("Неверный формат данных");
+
             } catch (NegativeNumberException e) {
-                System.out.println("Введена отрицательная цена");
+                System.out.println(e.getMessage());
             }
 
             System.out.println("Для завершения ввода товаров введите завершить, в противном случае любой символ");
-            String isClose = sc.next();
+            String isClose = scanner.next();
             if (isClose.trim().equalsIgnoreCase("завершить")) break;
         }
     }
--- src/main/java/Service.java
+++ src/main/java/Service.java
@@ -1,20 +1,29 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Service {
-    Scanner sc = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
     Calculator calculator = new Calculator();
 
     public void start() {
         System.out.println("Добро пожаловать");
         while (true) {
             System.out.println("На скольких человек разделить счет?");
-            int quantity = sc.nextInt();
+            int quantity = 0;
+            try {
+                quantity = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Введено некорректное значение");
+                scanner.next();
+                continue;
+            }
+
             if (quantity < 1) {
                 System.out.println("Введено некорректное количество гостей");
             } else if (quantity == 1) {
                 System.out.println("На одного гостя делить не надо");
             } else {
-                calculator.start(quantity, sc);
+                calculator.start(quantity, scanner);
                 break;
             }
 
